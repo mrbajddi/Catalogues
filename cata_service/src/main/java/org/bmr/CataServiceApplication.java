ab85b51f0a2ff1ad4f8812132b2a898533c6a783
Redouane BAJDDI <mr_bajddi@hotmail.fr>
2018-01-16 15:33:11 +0100

    add a comment to test GIT commit

--- cata_service/src/main/java/org/bmr/CataServiceApplication.java
+++ cata_service/src/main/java/org/bmr/CataServiceApplication.java
@@ -31,5 +31,7 @@ public class CataServiceApplication implements CommandLineRunner{
 	    
 	    List<Produit>  products =  produitRepository.findAll();
 	    products.forEach(p->System.out.println("designation : " + p.getDesignation() + " / "+ p.getPrix()));
+	    
+	    // added comment for GIT test
 	}
 }
