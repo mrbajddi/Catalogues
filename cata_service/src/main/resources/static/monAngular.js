b0e8baee5b13ca3fa28c946c6ac508d30264aa82
Warda <Warda@192.168.1.71>
2018-01-18 00:33:46 +0100

    enhance client

--- cata_service/src/main/resources/static/monAngular.js
+++ cata_service/src/main/resources/static/monAngular.js
@@ -2,11 +2,19 @@ var app = angular.module('MyApp', []);
 
 app.controller('MyController', function($scope, $http) {
 	$scope.pageProduits = null;
-
-	$http.get("http://localhost:8080//produits").success(function(data) {
-		$scope.pageProduits = data;
-	}).error(function(err) {
-		console.log(err);
-	});
-
+	
+	// Simple GET request example:
+	$http({
+	  method: 'GET',
+	  url: 'http://localhost:8080/produits'
+	}).then(function successCallback(response) {
+	    // this callback will be called asynchronously
+	    // when the response is available
+		$scope.pageProduits = response.data._embedded;
+	  }, function errorCallback(response) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+		  console.log(response);
+	  });
+	
 });
